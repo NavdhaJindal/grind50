4cf87f21386e16382cddc528b6fa8cf8ad8dfbac
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-02 23:33:10 -0700

    problem 2

--- 2-paranthesis.py
+++ 2-paranthesis.py
@@ -1,19 +1,38 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if ((s == '') or (s == '()') or (s == '{}') or (s == '[]')):
+        stack = []
+        hash_map = {'{':'}', '[':']', '(':')'}
+        for ch in s: 
+            if ch in hash_map:
+                stack.append(ch)
+            else:
+                if (stack == []):
+                    return False
+                elif (hash_map[stack[-1]] != ch):
+                    return False
+                else: 
+                    stack.pop()
+        if (stack == []): 
             return True
-        elif (len(s) <= 2):
+        else: 
             return False
-        else:
-            index = 0
-            while (index < len(s)):
-                if (((s[index] == '(') and (s[index+1] == ')') )
-                or ((s[index] == '{') and (s[index+1] == '}') )
-                or ((s[index] == '[') and (s[index+1] == ']') )):
-                    s = s[0:index] + s[index+2:]
-                    index -= 1
 
-                index +=1
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         if ((s == '') or (s == '()') or (s == '{}') or (s == '[]')):
+#             return True
+#         elif (len(s) <= 2):
+#             return False
+#         else:
+#             index = 0
+#             while (index < len(s)):
+#                 if (((s[index] == '(') and (s[index+1] == ')') )
+#                 or ((s[index] == '{') and (s[index+1] == '}') )
+#                 or ((s[index] == '[') and (s[index+1] == ']') )):
+#                     s = s[0:index] + s[index+2:]
+#                     index -= 1
 
-        print(s)
-        return self.isValid(s)
+#                 index +=1
+
+#         print(s)
+#         return self.isValid(s)
