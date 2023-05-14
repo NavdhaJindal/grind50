f1c8406bcfcee3ae866e831c4e03215359d99426
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-13 17:37:37 -0700

    2nd solution

--- 6-invertbinarytree.py
+++ 6-invertbinarytree.py
@@ -1,10 +1,22 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root == None:
+        if not root: 
             return None
-        temp = root.right
-        root.right = root.left
-        root.left = temp
-        self.invertTree(root.right)
-        self.invertTree(root.left)
+        stack = [root]
+        while (stack):
+            current_node = stack.pop()
+            if current_node:
+                temp = current_node.left
+                current_node.left = current_node.right
+                current_node.right = temp
+                stack.extend([current_node.left, current_node.right])
+
         return root
+        # if root == None:
+        #     return None
+        # temp = root.right
+        # root.right = root.left
+        # root.left = temp
+        # self.invertTree(root.right)
+        # self.invertTree(root.left)
+        # return root
