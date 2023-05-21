cf40e7284cf5fc52bc3efc9fb254ba99e5944f7d
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-20 23:14:12 -0700

    solution 2

--- 10-lowestcommonancestor.py
+++ 10-lowestcommonancestor.py
@@ -1,18 +1,27 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
 
-        smaller = min(p, q, key = lambda x: x.val)
-        larger = max(p, q, key = lambda x: x.val)
-#use the fact that they're binary search trees
+        while curr:
+            if curr.val > q.val and curr.val > p.val:
+                curr = curr.left
+            elif curr.val < q.val and curr.val < p.val:
+                curr = curr.right
+            else:
+                return curr
 
-        def lca(root, s, l):
-            if root:
-                if (s.val <= root.val and l.val >= root.val):
-                    return root
-                elif s.val <= root.val and l.val <= root.val:
-                    return self.lowestCommonAncestor(root.left, s, l)
-                else:
-                    return self.lowestCommonAncestor(root.right, s, l)
-            return root
+#         smaller = min(p, q, key = lambda x: x.val)
+#         larger = max(p, q, key = lambda x: x.val)
+# #use the fact that they're binary search trees
 
-        return lca(root, smaller, larger)
+#         def lca(root, s, l):
+#             if root:
+#                 if (s.val <= root.val and l.val >= root.val):
+#                     return root
+#                 elif s.val <= root.val and l.val <= root.val:
+#                     return self.lowestCommonAncestor(root.left, s, l)
+#                 else:
+#                     return self.lowestCommonAncestor(root.right, s, l)
+#             return root
+
+#         return lca(root, smaller, larger)
