fa8778e5c247c95a49914d74e2b4acf8601096a0
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-22 23:53:40 -0700

    final solution

--- 11-balancedbinarytree.py
+++ 11-balancedbinarytree.py
@@ -1,28 +1,44 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        ans = True
 
-
-        def height(root):
-            if not root:
+        def dfs(root):
+            nonlocal ans
+            if not root: 
                 return 0
             
-            left_height = height(root.left)
-            right_height = height(root.right)
+            l = dfs(root.left)
+            r = dfs(root.right)
 
-            if left_height == -1 or right_height == -1:
-                return -1
+            if abs(l - r) > 1: 
+                ans = False
+            return max(l, r) + 1
+        
+        dfs(root)
+        return ans
 
-            if (abs(right_height - left_height) > 1):
-                return -1
+
+        # def height(root):
+        #     if not root:
+        #         return 0
             
-            return max(height(root.left), height(root.right)) + 1
+        #     left_height = height(root.left)
+        #     right_height = height(root.right)
 
-        if not root: 
-            return True
-        if height(root) == -1: 
-            return False
-        else: 
-            return True
+        #     if left_height == -1 or right_height == -1:
+        #         return -1
+
+        #     if (abs(right_height - left_height) > 1):
+        #         return -1
+            
+        #     return max(height(root.left), height(root.right)) + 1
+
+        # if not root: 
+        #     return True
+        # if height(root) == -1: 
+        #     return False
+        # else: 
+        #     return True
 
         # def height(root):
         #     if not root:
