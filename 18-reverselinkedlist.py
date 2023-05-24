be4d6451afacb70005d6c85baaf0186c08b8d1e0
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-24 14:08:25 -0700

    in-place solution

--- 18-reverselinkedlist.py
+++ 18-reverselinkedlist.py
@@ -5,23 +5,37 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        while head:
-            stack.append(head)
-            head = head.next
+        if not (head and head.next):
+            return head  
+
+        prev_node = None
         
-        if len(stack) == 1: 
-            return stack[0]
-
-        if len(stack) < 1:
-            return None
-
-        new_head = stack.pop()
-
-        current_node = new_head
-        while stack:
-            current_node.next = stack.pop()
-            current_node = current_node.next
-            current_node.next = None
+        while head.next:
+            temp = head.next
+            head.next = prev_node
+            prev_node = head
+            head = temp
         
-        return new_head
+        head.next = prev_node
+
+        return head
+        # stack = []
+        # while head:
+        #     stack.append(head)
+        #     head = head.next
+        
+        # if len(stack) == 1: 
+        #     return stack[0]
+
+        # if len(stack) < 1:
+        #     return None
+
+        # new_head = stack.pop()
+
+        # current_node = new_head
+        # while stack:
+        #     current_node.next = stack.pop()
+        #     current_node = current_node.next
+        #     current_node.next = None
+        
+        # return new_head
