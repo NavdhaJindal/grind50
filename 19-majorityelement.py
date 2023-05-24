8bba0a8dcce343fd36bd16ca85eaa24a2de58f82
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-24 15:56:08 -0700

    voting algo solution

--- 19-majorityelement.py
+++ 19-majorityelement.py
@@ -1,7 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n_dict = {}
-        for n in nums:
-            n_dict[n] = n_dict.get(n, 0) + 1
-            if n_dict[n] > len(nums) // 2:
-                return n
+        candidate, count = nums[0], 0
+        for num in nums:
+            if num == candidate:
+                count += 1
+            elif count == 0:
+                candidate, count = num, 1
+            else:
+                count -= 1
+        return candidate
+    
+#         n_dict = {}
+#         for n in nums:
+#             n_dict[n] = n_dict.get(n, 0) + 1
+#             if n_dict[n] > len(nums) // 2:
+#                 return n
+
