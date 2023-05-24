92366c5b63ae628af1b62c92016c9a8881987446
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-23 20:31:36 -0700

    binary search solution

--- 14-firstbadversion.py
+++ 14-firstbadversion.py
@@ -3,6 +3,19 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        for i in range(1, n+1):
-            if isBadVersion(i):
-                return i
+        start = 1
+        end = n
+        min_version = n + 1
+
+        while start <= end:
+            current_version = (start + end)//2
+            if isBadVersion(current_version):
+                min_version = current_version
+                end = current_version - 1
+            elif not isBadVersion(current_version):
+                start = current_version + 1
+        return min_version
+
+        # for i in range(1, n+1):
+        #     if isBadVersion(i):
+        #         return i
