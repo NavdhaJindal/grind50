b13e25ce75a7eae11e006aad0fdc8e23a4dd791d
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-23 23:28:48 -0700

    fibonacci solution

--- 16-climbingstairs.py
+++ 16-climbingstairs.py
@@ -1,16 +1,33 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def factorial (num):
-            if num == 0 or num == 1:
-                return 1
-            return factorial(num - 1) * num
 
-        totalNum = 0
-        if n <= 0: 
+        if n == 0:
             return 0
+        if n == 1:
+            return 1
+        if n == 2: 
+            return 2
+        
+        steps_minus_1 = 1
+        steps_minus_2 = 2
+        for i in range(3, n+1):
+            current_steps = steps_minus_1 + steps_minus_2 
+            steps_minus_1 = steps_minus_2
+            steps_minus_2 = current_steps
+            
+        return current_steps
+    
+        # def factorial (num):
+        #     if num == 0 or num == 1:
+        #         return 1
+        #     return factorial(num - 1) * num
 
-        for step2s in range(0, n//2 + 1):
-            step1s = n - 2 * step2s
-            totalNum += factorial(step1s + step2s) // (factorial(step1s) * factorial(step2s))
+        # totalNum = 0
+        # if n <= 0: 
+        #     return 0
 
-        return totalNum
+        # for step2s in range(0, n//2 + 1):
+        #     step1s = n - 2 * step2s
+        #     totalNum += factorial(step1s + step2s) // (factorial(step1s) * factorial(step2s))
+
+        # return totalNum
