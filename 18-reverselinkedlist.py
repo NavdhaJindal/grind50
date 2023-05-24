d2ae27b70e25a268ccf8e1694528010459d01bb3
NavdhaJindal <jindalnavdha@gmail.com>
2023-05-24 12:53:30 -0700

    stack solution 1

---
+++ 18-reverselinkedlist.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack = []
+        while head:
+            stack.append(head)
+            head = head.next
+        
+        if len(stack) == 1: 
+            return stack[0]
+
+        if len(stack) < 1:
+            return None
+
+        new_head = stack.pop()
+
+        current_node = new_head
+        while stack:
+            current_node.next = stack.pop()
+            current_node = current_node.next
+            current_node.next = None
+        
+        return new_head
