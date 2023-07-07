23623b4aedbb8f7e9f373191f37032d8b54b87b1
NavdhaJindal <jindalnavdha@gmail.com>
2023-07-06 21:24:02 -0600

    solution

--- 26-kclosestpoints.py
+++ 26-kclosestpoints.py
@@ -1,11 +1,30 @@
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+#         for point in points:
+#             x, y = point
+#             dist = sqrt(math.pow(x, 2) + math.pow(y, 2))
+#             point.append(dist)
+
+#         points.sort(key = lambda x: x[2])
+
+#         return [[points[i][0], points[i][1]] for i in range(k)]
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        for point in points:
-            x, y = point
-            dist = sqrt(math.pow(x, 2) + math.pow(y, 2))
-            point.append(dist)
+        def calDistance(x: int, y: int):
+            return (pow(x, 2) + pow(y, 2))
 
-        points.sort(key = lambda x: x[2])
+        heap = []
 
-        return [[points[i][0], points[i][1]] for i in range(k)]
+        for x,y in points: 
+            heapq.heappush(heap, (calDistance(x,y), x, y))
+        
+        k_closest_points = []
+
+        for i in range(k):
+            d, x, y = heapq.heappop(heap)
+            k_closest_points.append([x, y])
+
+        return k_closest_points
