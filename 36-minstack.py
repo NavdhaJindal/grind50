20b3d72f92c436fd5f401263f544bdbdaf78d169
NavdhaJindal <jindalnavdha@gmail.com>
2023-07-31 18:21:55 -0600

    improper solution - not O(1)

---
+++ 36-minstack.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.heap = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        heapq.heappush(self.heap, val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.heap = [i for i in self.stack]
+        heapq.heapify(self.heap)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        min_element = heapq.heappop(self.heap)
+        heapq.heappush(self.heap, min_element)
+        return min_element
