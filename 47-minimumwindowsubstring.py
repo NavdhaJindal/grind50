1cdbc90283a1f1844f7f18d562dcdc03f527541d
NavdhaJindal <jindalnavdha@gmail.com>
2023-08-29 11:34:41 -0600

    neetcode soln

--- 47-minimumwindowsubstring.py
+++ 47-minimumwindowsubstring.py
@@ -1,53 +1,88 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        char_map = {}
-        for ch in t: 
-            char_map[ch] = char_map.get(ch, 0) + 1
-
-        need = len(char_map)
-        start, end = 0, 0
-        final_start = len(s) + 1
-        min_len = len(s) + 1 
-        copy_map = {}
-
-        while end < len(s):
-            print("top of loop end is ", end)
-            ch = s[end]
-
-            if ch in char_map: 
-                copy_map[ch] = copy_map.get(ch, 0) + 1
-                if char_map[ch] == copy_map[ch]:
-                    need -= 1
-                    print("found a match in ending char ", ch, ", updating need to ", need)
-                    if need == 0:
-                        # min_len = min(min_len, end - start + 1)
-                        if end - start + 1 < min_len: 
-                            min_len = end - start + 1
-                            final_start = start
-                            print('final start has been updated to ', final_start, ' as need is now 0')
-                        while start < end:
-                            print("top of inner loop start is ", start)
-                            ch2 = s[start]
-                            if ch2 in copy_map: 
-                                copy_map[ch2] -= 1
-                                if char_map[ch2] != copy_map[ch2]:
-                                    start += 1
-                                    need += 1
-                                    print("found a match in starting char ", ch2, ", updating need to ", need)
-                                    break
-                            start += 1
-                            final_start = start
-                            print('final start has been updated to ', final_start, ' as superfluos chars')
-                            min_len = min(min_len, end - start + 1)
-
-
-            end += 1
-
-        if min_len == len(s) + 1: 
+        if t == "":
             return ""
 
-        return s[final_start:final_start + min_len]
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                
+                # update our result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # pop from the left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
+
+
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+
+#         char_map = {}
+#         for ch in t: 
+#             char_map[ch] = char_map.get(ch, 0) + 1
+
+#         need = len(char_map)
+#         start, end = 0, 0
+#         final_start = len(s) + 1
+#         min_len = len(s) + 1 
+#         copy_map = {}
+
+#         while end < len(s):
+#             print("top of loop end is ", end)
+#             ch = s[end]
+
+#             if ch in char_map: 
+#                 copy_map[ch] = copy_map.get(ch, 0) + 1
+#                 if char_map[ch] == copy_map[ch]:
+#                     need -= 1
+#                     print("found a match in ending char ", ch, ", updating need to ", need)
+#                     if need == 0:
+#                         # min_len = min(min_len, end - start + 1)
+#                         if end - start + 1 < min_len: 
+#                             min_len = end - start + 1
+#                             final_start = start
+#                             print('final start has been updated to ', final_start, ' as need is now 0')
+#                         while start < end:
+#                             print("top of inner loop start is ", start)
+#                             ch2 = s[start]
+#                             if ch2 in copy_map: 
+#                                 copy_map[ch2] -= 1
+#                                 if char_map[ch2] != copy_map[ch2]:
+#                                     start += 1
+#                                     need += 1
+#                                     print("found a match in starting char ", ch2, ", updating need to ", need)
+#                                     break
+#                             start += 1
+#                             final_start = start
+#                             print('final start has been updated to ', final_start, ' as superfluos chars')
+#                             min_len = min(min_len, end - start + 1)
+
+
+#             end += 1
+
+#         if min_len == len(s) + 1: 
+#             return ""
+
+#         return s[final_start:final_start + min_len]
 
 
 # class Solution:
